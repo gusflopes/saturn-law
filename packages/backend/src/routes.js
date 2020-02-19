65b0d1a2a908798bbe1408f064e35613fc7265df
gusflopes <gusflopes86@gmail.com>
2020-02-19 00:43:50 -0400

    feat: changing pattern

--- packages/backend/src/routes.js
+++ packages/backend/src/routes.js
@@ -11,6 +11,8 @@ routes.use('/users', require('./routes/users'));
 routes.use('/lawfirms/members', require('./routes/lawfirmMembers.js'));
 routes.use('/lawfirms', require('./routes/lawfirms'));
 
+routes.use('/v1', require('./routes/v1/users', './routes/v1/hello'));
+
 // middlewares
 
 // Private Routes
